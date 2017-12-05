a36bc532804bd882e6224e58207408cfb2db8ad3
PratikDutta <iampratikdutta.29@gmail.com>
2017-12-05 08:45:30 +0530

    PreOrderTraversal added

--- Trees/src/home/pratik/datastructures/BinaryTree.java
+++ Trees/src/home/pratik/datastructures/BinaryTree.java
@@ -1,34 +1,55 @@
 package home.pratik.datastructures;
 
+import org.apache.log4j.Logger;
+
 import com.sun.media.sound.InvalidDataException;
 
 public class BinaryTree<E> {
+	
+	private static final Logger LOG = Logger.getLogger(BinaryTree.class);
 
 	public String levelOrderTraversal(BinaryTreeNode<E> rootNode) throws InvalidDataException{
-		LinkedList<E> list = null;
+		LinkedList<E> levelOrderList = null;
 		LinkedListedQueue<BinaryTreeNode<E>> queue = null;
 		BinaryTreeNode<E> currentNode = null;
 		if(rootNode != null){
-			list = new LinkedList<>();
+			levelOrderList = new LinkedList<>();
 			queue = new LinkedListedQueue<>();
 			queue.add(rootNode);
 			while(!queue.isEmpty()){
 				currentNode = queue.poll();
-				list.addAfterTail(currentNode.data);
+				levelOrderList.addAfterTail(currentNode.data);
 				queue.add(currentNode.leftChild);
 				queue.add(currentNode.rightChild);
 			}
-			return list.toString();
+			return levelOrderList.toString();
 		}
 		return null;
 	}
 	
 	
 	public String preOrderTraversal(BinaryTreeNode<E> rootNode){
-		
-		
-		
+		LinkedList<E> preOrderList = null;
+		NodedStack<BinaryTreeNode<E>> stack = null;
+		BinaryTreeNode<E> currentNode = null;
+		if(rootNode != null){
+			preOrderList = new LinkedList<>();
+			stack = new NodedStack<>(rootNode);
+			while(!stack.empty()){
+				currentNode = stack.pop();
+				try {
+					if(currentNode != null){
+						preOrderList.addAfterTail(currentNode.data);
+						stack.push(currentNode.rightChild);
+						stack.push(currentNode.leftChild);
+					}
+				} catch (InvalidDataException e) {
+					LOG.error("Exception Occured : ", e);
+				}
+			}
+		}
+		if(preOrderList != null)
+			return preOrderList.toString();
 		return null;
 	}
-	
 }
---
+++ Trees/src/home/pratik/traversals/PreOrderTraversalTester.java
@@ -0,0 +1,32 @@
+package home.pratik.traversals;
+
+import org.apache.log4j.Logger;
+
+import home.pratik.datastructures.BinaryTree;
+import home.pratik.datastructures.BinaryTreeNode;
+
+public class PreOrderTraversalTester {
+
+	private static final Logger LOG = Logger.getLogger(PreOrderTraversalTester.class);
+	
+	public static void main(String[] args) {
+		BinaryTreeNode<Integer> intRootNode = new BinaryTreeNode<>(12);
+		intRootNode.setLeftChild(new BinaryTreeNode<>(9));
+		intRootNode.setRightChild(new BinaryTreeNode<>(14));
+		intRootNode.getLeftChild().setLeftChild(new BinaryTreeNode<>(2));
+		intRootNode.getLeftChild().setRightChild(new BinaryTreeNode<>(10));
+		intRootNode.getRightChild().setLeftChild(new BinaryTreeNode<>(13));
+		intRootNode.getRightChild().setRightChild(new BinaryTreeNode<>(16));
+		intRootNode.getLeftChild().getLeftChild().setLeftChild(new BinaryTreeNode<>(1));
+		intRootNode.getLeftChild().getRightChild().setLeftChild(new BinaryTreeNode<>(8));
+		intRootNode.getLeftChild().getRightChild().setRightChild(new BinaryTreeNode<>(11));
+		intRootNode.getRightChild().getRightChild().setLeftChild(new BinaryTreeNode<>(15));
+		intRootNode.getRightChild().getRightChild().setRightChild(new BinaryTreeNode<>(18));
+		intRootNode.getRightChild().getRightChild().getRightChild().setLeftChild(new BinaryTreeNode<>(17));
+		intRootNode.getRightChild().getRightChild().getRightChild().setRightChild(new BinaryTreeNode<>(20));
+		
+		BinaryTree<Integer> intsTree = new BinaryTree<>();
+		String traversalString = intsTree.preOrderTraversal(intRootNode);
+		LOG.info("Pre-Order-Traversal String : " + traversalString);
+	}
+}
