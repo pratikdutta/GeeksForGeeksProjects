44996893e007ca9134550e0e3e6844f507dfb1c8
PratikDutta <iampratikdutta.29@gmail.com>
2018-03-14 08:36:49 +0530

    New Project: Solved Examples and updated .gitignore files

--- LinkedLists/src/home/linkedlist/implementations/ReverseLinkedListInSpecifiedSize.java
+++ LinkedLists/src/home/linkedlist/implementations/ReverseLinkedListInSpecifiedSize.java
@@ -1,38 +1,35 @@
 package home.linkedlist.implementations;
 
+import com.sun.media.sound.InvalidDataException;
+
 import home.pratik.linkedlist.LinkedList;
 import home.pratik.linkedlist.Node;
 
 public class ReverseLinkedListInSpecifiedSize {
 
 	public static void main(String[] args) {
-		
-	}
-	
-	private <E> void getReversedLinkedList(LinkedList<E> list, int reversingLength){
-		LinkedList<E> tempList = null; 
-		if(list != null && reversingLength > 0){
-			int iterration = 0;
-			Node<E> current = list.getHead();
-			while(current != null){
-				tempList = new LinkedList<>(); 
-				Node<E> lenNode = current;
-				while(lenNode != null && iterration < reversingLength){
-					lenNode = lenNode.getNext();
-					tempList.addNodeAfterTail(lenNode);
-					iterration++;
-				}
-				reverse(tempList);
-				current = lenNode.getNext();
-				
-				lenNode.setNext(null);
-			}
+		LinkedList<Integer> intsList = new LinkedList<>();
+		try {
+			intsList.addAfterTail(1);
+			intsList.addAfterTail(2);
+			intsList.addAfterTail(3);
+			intsList.addAfterTail(4);
+			intsList.addAfterTail(5);
+			intsList.addAfterTail(6);
+			intsList.addAfterTail(7);
+			intsList.addAfterTail(8);
+			intsList.addAfterTail(9);
+			intsList.addAfterTail(10);
+			getReversedLinkedList(intsList, 3);
+			System.out.println(intsList);
+		} catch (InvalidDataException e) {
 		}
 	}
 	
-	private <E> void reverse(LinkedList<E> subList){
-		if(subList != null){
-			subList.reverse();
+	private static void getReversedLinkedList(LinkedList<Integer> list, int reversingLength){
+		Node<Integer> altFirst = null;
+		for(int i=0; i<list.getSize(); i++){
+			
 		}
 	}
 }
